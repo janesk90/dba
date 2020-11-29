5624716d56fffa5d4f10c777108d196c4820afb3
janesk90 <janesk90@gmail.com>
2020-11-29 14:21:47 -0500

    I really need someone to review this

--- functions.sql
+++ functions.sql
@@ -1,3 +1,4 @@
+-- I think this is right? I need someone to review this as I'm completely in the dark; I just know how to model data tbh
 DELIMITER |
 CREATE FUNCTION addCustomer(
 	firstname VARCHAR(100),
@@ -7,6 +8,7 @@ CREATE FUNCTION addCustomer(
 RETURNS INT
 BEGIN
 	INSERT INTO customers (customers_firstname, customers_lastname, customers_email) VALUES (firstname, lastname, email);
+    RETURN 1;
 END;
 CREATE FUNCTION addProduct products(
 	_name VARCHAR(100),
@@ -19,5 +21,15 @@ RETURNS INT
 BEGIN
 	INSERT INTO products (products_name, products_price, products_stock, products_description, products_restock_level) 
     VALUES (_name, _price, _stock, _description, _restock_level);
+    RETURN 1;
 END;
+CREATE FUNCTION findProductsCategories(
+	_products_id
+)
+RETURNS INT
+BEGIN
+	SELECT categories.categories_id, categories.categories_name
+    INTO @catid, @catname
+    FROM products_to_categories JOIN categories WHERE products_id = _products_id;
+END
 DELIMITER ;
