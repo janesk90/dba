e9c550c1c2dbec946e2bb43326b3faf003056482
Dylanshapiro <43762253+Dylanshapiro@users.noreply.github.com>
2020-12-01 23:12:29 -0500

    Update AllProducts.sql

--- AllProducts.sql
+++ AllProducts.sql
@@ -1,9 +1,10 @@
-CREATE
-    ALGORITHM = UNDEFINED
-    DEFINER = `shapirod1`@`%`
+CREATE 
+    ALGORITHM = UNDEFINED 
+    DEFINER = `shapirod1`@`%` 
     SQL SECURITY DEFINER
 VIEW `AllProducts` AS
-    SELECT
+    SELECT 
+        `film_list`.`FID` AS `ProductID`,
         `film_list`.`title` AS `Name`,
         `film_list`.`description` AS `Description`,
         `film_list`.`category` AS `Category`,
@@ -16,8 +17,9 @@ VIEW `AllProducts` AS
                 `film_list`.`length`) AS `MoreInfo`,
         'sakila' AS `Provider`
     FROM
-        `sakila`.`film_list`
-    UNION SELECT
+        `sakila`.`film_list` 
+    UNION SELECT 
+        `adventureworks`.`product`.`ProductID` AS `ProductID`,
         `adventureworks`.`product`.`Name` AS `Name`,
         CONCAT('TODO') AS `Description`,
         CONCAT('TODO') AS `Category`,
@@ -28,8 +30,9 @@ VIEW `AllProducts` AS
                 `adventureworks`.`product`.`Size`) AS `MoreInfo`,
         'adventureworks' AS `Provider`
     FROM
-        `adventureworks`.`product`
-    UNION SELECT
+        `adventureworks`.`product` 
+    UNION SELECT 
+        `northwind`.`products`.`id` AS `ProductID`,
         `northwind`.`products`.`product_name` AS `Name`,
         `northwind`.`products`.`description` AS `Description`,
         `northwind`.`products`.`category` AS `Category`,
@@ -37,13 +40,16 @@ VIEW `AllProducts` AS
         CONCAT('N/A') AS `MoreInfo`,
         'northwind' AS `Provider`
     FROM
-        `northwind`.`products`
-    UNION SELECT
+        `northwind`.`products` 
+    UNION SELECT 
+        `products`.`products_id` AS `ProductID`,
         `products`.`products_name` AS `Name`,
         `products`.`products_description` AS `Description`,
-        `products`.`products_category` AS `Category`,
+        `categories`.`categories_name` AS `Category`,
         `products`.`products_price` AS `Price`,
         CONCAT('N/A') AS `MoreInfo`,
         'AMAZON2.0' AS `Provider`
     FROM
-        `products`
+        ((`products`
+        JOIN `products_to_categories` ON ((`products`.`products_id` = `products_to_categories`.`products_id`)))
+        JOIN `categories` ON ((`products_to_categories`.`categories_id` = `categories`.`categories_id`)))
