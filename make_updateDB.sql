76704b8a7313497e2e79b92e949c4f7ba273719b
Dylanshapiro <43762253+Dylanshapiro@users.noreply.github.com>
2020-12-01 22:16:18 -0500

    Create make_updateDB.sql

---
+++ make_updateDB.sql
@@ -0,0 +1,7 @@
+source tables.sql
+source AllProducts.sql
+source functions_customer/add_customer.sql
+source functions_customer/set_customer_active.sql
+source functions_customer/update_customer.sql
+source AddProducts.sql
+source AddCustomers.sql
